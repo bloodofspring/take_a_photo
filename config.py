8fe8b83282091da8d24502c8aadf71329c1135b9
bloodofspring <t.shagin@gmail.com>
2024-10-26 22:43:12 +0300

    feat: ID владельца перенесен в .env, код протестирован и отформатирован по PEP-8

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-from typing import Final
-from dotenv import load_dotenv
 from os import environ
+from typing import Final
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
-OWNER_ID: Final[int] = environ["owner_id"]
+OWNER_ID: Final[int] = int(environ["owner_id"])
